f6a33ae6ccd0574e2264cf1ca069520e043d3e22
Tony Arcieri <bascule@gmail.com>
2019-09-19 09:12:59 -0600

    polyval: Simplify implementation
    Eliminate needless traits and module depth, making `field::Element` into
    a simple newtype wrapper.

--- polyval/src/field/backend.rs
+++ polyval/src/field/backend.rs
@@ -1,37 +1,6 @@
 //! Field arithmetic backends
 
-#[cfg(all(
-    target_feature = "pclmulqdq",
-    target_feature = "sse2",
-    target_feature = "sse4.1",
-    any(target_arch = "x86", target_arch = "x86_64")
-))]
-mod pclmulqdq;
-mod soft;
 
-use super::Block;
-use core::ops::{Add, Mul};
-
-// TODO(tarcieri): runtime selection of PCLMULQDQ based on CPU features
-
-#[cfg(all(
-    target_feature = "pclmulqdq",
-    target_feature = "sse2",
-    target_feature = "sse4.1",
-    any(target_arch = "x86", target_arch = "x86_64")
-))]
-pub(crate) use self::pclmulqdq::M128i;
-
-#[allow(unused_imports)]
-pub(crate) use self::soft::U64x2;
-
-#[cfg(not(all(
-    target_feature = "pclmulqdq",
-    target_feature = "sse2",
-    target_feature = "sse4.1",
-    any(target_arch = "x86", target_arch = "x86_64")
-)))]
-pub(crate) type M128i = U64x2;
 
 /// Field arithmetic backend
 pub trait Backend:
