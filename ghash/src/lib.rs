2ed89f11bba4b9e94290c9dc3ba192c27758452e
Артём Павлов <newpavlov@gmail.com>
2020-07-24 18:23:50 +0300

    GHASH rework

--- ghash/src/lib.rs
+++ ghash/src/lib.rs
@@ -20,18 +20,15 @@
 //! > that these irreducible polynomials are the "reverse" of each other.
 //!
 //! [`polyval`]: https://github.com/RustCrypto/universal-hashes/tree/master/polyval
-
-#![no_std]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
 #![warn(missing_docs, rust_2018_idioms)]
 
-pub use polyval::universal_hash;
-
-use core::convert::TryInto;
-use polyval::Polyval;
+pub use universal_hash;
 use universal_hash::{consts::U16, NewUniversalHash, UniversalHash};
 #[cfg(feature = "zeroize")]
 use zeroize::Zeroize;
+
+use core::arch::x86_64::*;
 
 /// GHASH keys (16-bytes)
 pub type Key = universal_hash::Key<GHash>;
@@ -48,74 +45,102 @@ pub type Tag = universal_hash::Output<GHash>;
 /// the AES-GCM authenticated encryption cipher.
 #[derive(Clone)]
 #[repr(align(16))]
-pub struct GHash(Polyval);
+pub struct GHash {
+    h: __m128i,
+    y: __m128i,
+}
+
+const BS_MASK1: i64 = 0x00010203_04050607;
+const BS_MASK2: i64 = 0x08090A0B_0C0D0E0F;
 
 impl NewUniversalHash for GHash {
     type KeySize = U16;
 
     /// Initialize GHASH with the given `H` field element
     fn new(h: &Key) -> Self {
-        let mut h = *h;
-        h.reverse();
-
-        #[allow(unused_mut)]
-        let mut h_polyval = mulX_POLYVAL(&h);
-
-        #[cfg(feature = "zeroize")]
-        h.zeroize();
-
-        #[allow(clippy::let_and_return)]
-        let result = GHash(Polyval::new(&h_polyval));
-
-        #[cfg(feature = "zeroize")]
-        h_polyval.zeroize();
-
-        result
+        unsafe {
+            let bs_mask = _mm_set_epi64x(BS_MASK1, BS_MASK2);
+            let h = _mm_loadu_si128(h.as_ptr() as *const _);
+            let h = _mm_shuffle_epi8(h, bs_mask);
+            let y = _mm_setzero_si128();
+            Self { h, y }
+        }
     }
 }
 
 impl UniversalHash for GHash {
     type BlockSize = U16;
 
-    /// Input a field element `X` to be authenticated
+    #[inline]
     fn update(&mut self, x: &Block) {
-        let mut x = *x;
-        x.reverse();
-        self.0.update(&x);
+        unsafe {
+            let bs_mask = _mm_set_epi64x(BS_MASK1, BS_MASK2);
+
+            let h = self.h;
+            let x = _mm_loadu_si128(x.as_ptr() as *const _);
+            let x = _mm_shuffle_epi8(x, bs_mask);
+            let y = _mm_xor_si128(self.y, x);
+
+            let h0 = h;
+            let h1 = _mm_shuffle_epi32(h, 0x0E);
+            let h2 = _mm_xor_si128(h0, h1);
+            let y0 = y;
+            let y1 = _mm_shuffle_epi32(y, 0x0E);
+            let y2 = _mm_xor_si128(y0, y1);
+            let t0 = _mm_clmulepi64_si128(y0, h0, 0x00);
+            let t1 = _mm_clmulepi64_si128(y, h, 0x11);
+            let t2 = _mm_clmulepi64_si128(y2, h2, 0x00);
+            let t2 = _mm_xor_si128(t2, _mm_xor_si128(t0, t1));
+            let v0 = t0;
+            let v1 = _mm_xor_si128(_mm_shuffle_epi32(t0, 0x0E), t2);
+            let v2 = _mm_xor_si128(t1, _mm_shuffle_epi32(t2, 0x0E));
+            let v3 = _mm_shuffle_epi32(t1, 0x0E);
+
+            let v3 = _mm_or_si128(_mm_slli_epi64(v3, 1), _mm_srli_epi64(v2, 63));
+            let v2 = _mm_or_si128(_mm_slli_epi64(v2, 1), _mm_srli_epi64(v1, 63));
+            let v1 = _mm_or_si128(_mm_slli_epi64(v1, 1), _mm_srli_epi64(v0, 63));
+            let v0 = _mm_slli_epi64(v0, 1);
+
+            let v2 = _mm_xor_si128(
+                v2,
+                _mm_xor_si128(
+                    _mm_xor_si128(v0, _mm_srli_epi64(v0, 1)),
+                    _mm_xor_si128(_mm_srli_epi64(v0, 2), _mm_srli_epi64(v0, 7)),
+                ),
+            );
+            let v1 = _mm_xor_si128(
+                _mm_xor_si128(v1, _mm_slli_epi64(v0, 63)),
+                _mm_xor_si128(_mm_slli_epi64(v0, 62), _mm_slli_epi64(v0, 57)),
+            );
+            let v3 = _mm_xor_si128(
+                v3,
+                _mm_xor_si128(
+                    _mm_xor_si128(v1, _mm_srli_epi64(v1, 1)),
+                    _mm_xor_si128(_mm_srli_epi64(v1, 2), _mm_srli_epi64(v1, 7)),
+                ),
+            );
+            let v2 = _mm_xor_si128(
+                _mm_xor_si128(v2, _mm_slli_epi64(v1, 63)),
+                _mm_xor_si128(_mm_slli_epi64(v1, 62), _mm_slli_epi64(v1, 57)),
+            );
+
+            self.y = _mm_unpacklo_epi64(v2, v3);
+        }
     }
 
     /// Reset internal state
     fn reset(&mut self) {
-        self.0.reset();
+        unsafe {
+            self.y = _mm_setzero_si128();
+        }
     }
 
     /// Get GHASH output
     fn finalize(self) -> Tag {
-        let mut output = self.0.finalize().into_bytes();
-        output.reverse();
-        Tag::new(output)
+        unsafe {
+            let bs_mask = _mm_set_epi64x(BS_MASK1, BS_MASK2);
+            core::mem::transmute(_mm_shuffle_epi8(self.y, bs_mask))
+        }
     }
 }
 
-/// The `mulX_POLYVAL()` function as defined in [RFC 8452 Appendix A][1].
-/// Performs a doubling (a.k.a. "multiply by x") over GF(2^128).
-/// This is useful for implementing GHASH in terms of POLYVAL.
-///
-/// [1]: https://tools.ietf.org/html/rfc8452#appendix-A
-#[allow(non_snake_case)]
-fn mulX_POLYVAL(block: &Block) -> Block {
-    let mut v0 = u64::from_le_bytes(block[..8].try_into().unwrap());
-    let mut v1 = u64::from_le_bytes(block[8..].try_into().unwrap());
-
-    let v0h = v0 >> 63;
-    let v1h = v1 >> 63;
-
-    v0 <<= 1;
-    v1 <<= 1;
-    v0 ^= v1h;
-    v1 ^= v0h ^ (v1h << 63) ^ (v1h << 62) ^ (v1h << 57);
-
-    (u128::from(v0) | (u128::from(v1) << 64))
-        .to_le_bytes()
-        .into()
-}
