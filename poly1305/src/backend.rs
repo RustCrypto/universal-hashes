cd5508cb2a816b52562686e9c6063d9a1b2cd8e4
Tony Arcieri <bascule@gmail.com>
2020-12-09 13:55:46 -0800

    poly1305: AVX2 detection (#97)
    * poly1305: AVX2 detection
    
    Automatically detects the availability of CLMUL based on CPUID, and
    falls back to the "soft" implementation if unavailable.
    
    * poly1305: apply #[target_feature(enable="avx2")]
    
    * Extract problematic lambdas into named functions
    
    * poly1305: impl Zeroize for autodetect::State

--- poly1305/src/backend.rs
+++ poly1305/src/backend.rs
@@ -1,27 +1,9 @@
+//! Poly1305 backends
+
 #[cfg(all(
     any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "avx2"
+    not(feature = "force-soft")
 ))]
 pub(crate) mod avx2;
 
-#[cfg(any(
-    not(all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        target_feature = "avx2"
-    )),
-    fuzzing,
-    test,
-))]
 pub(crate) mod soft;
-
-#[cfg(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "avx2",
-))]
-pub(crate) use avx2::State;
-
-#[cfg(not(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "avx2",
-)))]
-pub(crate) use soft::State;
