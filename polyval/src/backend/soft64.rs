d3f7b355c4c34e152d58dd6cebe10e1c9eeea511
Tony Arcieri <bascule@gmail.com>
2022-07-20 08:44:44 -0600

    polyval: impl `Reset` (#157)
    This is useful in the implementation of `aes-gcm-siv`, as it allows for
    calling the newly re-added `finalize_reset` method, which is useful to
    avoid consuming `self` when computing the tag.
    
    SIV mode encryption requires first computing the tag as a pass of
    POLYVAL over the plaintext, where the tag also functions as a synthetic
    initialization vector. With AES-GCM-SIV specifically, it also needs
    access to the original nonce, which is XORed into the tag.
    
    Consuming `self` means it's not possible to store the cipher, POLYVAL,
    and nonce in the same struct, since after computing the SIV tag it needs
    to be passed to the cipher.

--- polyval/src/backend/soft64.rs
+++ polyval/src/backend/soft64.rs
@@ -13,7 +13,7 @@ use core::{
 use universal_hash::{
     consts::{U1, U16},
     crypto_common::{BlockSizeUser, KeySizeUser, ParBlocksSizeUser},
-    KeyInit, UhfBackend, UniversalHash,
+    KeyInit, Reset, UhfBackend, UniversalHash,
 };
 
 #[cfg(feature = "zeroize")]
@@ -75,6 +75,12 @@ impl UniversalHash for Polyval {
         }
 
         block
+    }
+}
+
+impl Reset for Polyval {
+    fn reset(&mut self) {
+        self.s = U64x2::default();
     }
 }
 
