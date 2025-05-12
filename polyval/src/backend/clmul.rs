e8254cfbf16993bf8e16bd55ea19919392edcfdf
Nick Mathewson <nickm@torproject.org>
2025-05-12 11:39:26 -0400

    polyval: Use clmul backend from Eric Lagergren
    (No pipelining yet.)
    
    Adopted with permission.  Mostly copied from
    https://github.com/ericlagergren/polyval-rs, with minor adaptations.
    
    Before:
    
    ```
    test bench1_10    ... bench:           8.84 ns/iter (+/- 0.15) = 1250 MB/s
    test bench2_100   ... bench:          43.47 ns/iter (+/- 0.19) = 2325 MB/s
    test bench3_1000  ... bench:         369.67 ns/iter (+/- 2.51) = 2710 MB/s
    test bench3_10000 ... bench:       3,661.72 ns/iter (+/- 20.97) = 2731 MB/s
    ```
    
    After:
    
    ```
    test bench1_10    ... bench:           8.14 ns/iter (+/- 0.07) = 1250 MB/s
    test bench2_100   ... bench:          37.34 ns/iter (+/- 0.09) = 2702 MB/s
    test bench3_1000  ... bench:         311.00 ns/iter (+/- 3.48) = 3225 MB/s
    test bench3_10000 ... bench:       3,080.22 ns/iter (+/- 137.46) = 3246 MB/s
    ```
    
    Based-On-Code-By: Eric Lagergren <5894247+ericlagergren@users.noreply.github.com>

--- polyval/src/backend/clmul.rs
+++ polyval/src/backend/clmul.rs
@@ -1,5 +1,8 @@
 //! Intel `CLMUL`-accelerated implementation for modern x86/x86_64 CPUs
 //! (i.e. Intel Sandy Bridge-compatible or newer)
+//!
+//! Based on implementation by Eric Lagergren
+//! at <https://github.com/ericlagergren/polyval-rs/>.
 
 #[cfg(target_arch = "x86")]
 use core::arch::x86::*;
@@ -13,6 +16,7 @@ use universal_hash::{
 };
 
 use crate::{Block, Key, Tag};
+use core::ptr;
 
 /// **POLYVAL**: GHASH-like universal hash over GF(2^128).
 #[derive(Clone)]
@@ -63,7 +67,7 @@ impl UhfBackend for Polyval {
 }
 
 impl Polyval {
-    /// Get GHASH output
+    /// Get Polyval output
     pub(crate) fn finalize(self) -> Tag {
         unsafe { core::mem::transmute(self.y) }
     }
@@ -74,75 +78,8 @@ impl Polyval {
     #[target_feature(enable = "pclmulqdq")]
     #[allow(unsafe_op_in_unsafe_fn)]
     unsafe fn mul(&mut self, x: &Block) {
-        #[inline(always)]
-        unsafe fn xor4(e1: __m128i, e2: __m128i, e3: __m128i, e4: __m128i) -> __m128i {
-            _mm_xor_si128(_mm_xor_si128(e1, e2), _mm_xor_si128(e3, e4))
-        }
-
-        #[inline(always)]
-        unsafe fn xor5(e1: __m128i, e2: __m128i, e3: __m128i, e4: __m128i, e5: __m128i) -> __m128i {
-            _mm_xor_si128(
-                e1,
-                _mm_xor_si128(_mm_xor_si128(e2, e3), _mm_xor_si128(e4, e5)),
-            )
-        }
-
-        let h = self.h;
-
-        // `_mm_loadu_si128` performs an unaligned load
-        #[allow(clippy::cast_ptr_alignment)]
-        let x = _mm_loadu_si128(x.as_ptr() as *const __m128i);
-        let y = _mm_xor_si128(self.y, x);
-
-        let h0 = h;
-        let h1 = _mm_shuffle_epi32(h, 0x0E);
-        let h2 = _mm_xor_si128(h0, h1);
-        let y0 = y;
-
-        // Multiply values partitioned to 64-bit parts
-        let y1 = _mm_shuffle_epi32(y, 0x0E);
-        let y2 = _mm_xor_si128(y0, y1);
-        let t0 = _mm_clmulepi64_si128(y0, h0, 0x00);
-        let t1 = _mm_clmulepi64_si128(y, h, 0x11);
-        let t2 = _mm_clmulepi64_si128(y2, h2, 0x00);
-        let t2 = _mm_xor_si128(t2, _mm_xor_si128(t0, t1));
-        let v0 = t0;
-        let v1 = _mm_xor_si128(_mm_shuffle_epi32(t0, 0x0E), t2);
-        let v2 = _mm_xor_si128(t1, _mm_shuffle_epi32(t2, 0x0E));
-        let v3 = _mm_shuffle_epi32(t1, 0x0E);
-
-        // Polynomial reduction
-        let v2 = xor5(
-            v2,
-            v0,
-            _mm_srli_epi64(v0, 1),
-            _mm_srli_epi64(v0, 2),
-            _mm_srli_epi64(v0, 7),
-        );
-
-        let v1 = xor4(
-            v1,
-            _mm_slli_epi64(v0, 63),
-            _mm_slli_epi64(v0, 62),
-            _mm_slli_epi64(v0, 57),
-        );
-
-        let v3 = xor5(
-            v3,
-            v1,
-            _mm_srli_epi64(v1, 1),
-            _mm_srli_epi64(v1, 2),
-            _mm_srli_epi64(v1, 7),
-        );
-
-        let v2 = xor4(
-            v2,
-            _mm_slli_epi64(v1, 63),
-            _mm_slli_epi64(v1, 62),
-            _mm_slli_epi64(v1, 57),
-        );
-
-        self.y = _mm_unpacklo_epi64(v2, v3);
+        let x = _mm_loadu_si128(x.as_ptr().cast());
+        self.y = polymul(_mm_xor_si128(self.y, x), self.h);
     }
 }
 
@@ -161,4 +98,137 @@ impl Drop for Polyval {
         self.h.zeroize();
         self.y.zeroize();
     }
+}
+
+/// # Safety
+///
+/// The SSE2 and pclmulqdq target features must be enavled.
+#[inline]
+#[target_feature(enable = "sse2,pclmulqdq")]
+#[allow(unused_unsafe)]
+#[allow(clippy::undocumented_unsafe_blocks, reason = "Too many unsafe blocks.")]
+unsafe fn polymul(x: __m128i, y: __m128i) -> __m128i {
+    let (h, m, l) = unsafe { karatsuba1(x, y) };
+    let (h, l) = unsafe { karatsuba2(h, m, l) };
+    unsafe {
+        mont_reduce(h, l) // d
+    }
+}
+
+/// Karatsuba decomposition for `x*y`.
+#[inline]
+#[target_feature(enable = "sse2,pclmulqdq")]
+#[allow(unused_unsafe)]
+#[allow(clippy::undocumented_unsafe_blocks, reason = "Too many unsafe blocks.")]
+unsafe fn karatsuba1(x: __m128i, y: __m128i) -> (__m128i, __m128i, __m128i) {
+    // First Karatsuba step: decompose x and y.
+    //
+    // (x1*y0 + x0*y1) = (x1+x0) * (y1+x0) + (x1*y1) + (x0*y0)
+    //        M                                 H         L
+    //
+    // m = x.hi^x.lo * y.hi^y.lo
+    let m = unsafe {
+        pmull(
+            _mm_xor_si128(x, _mm_shuffle_epi32(x, 0xee)),
+            _mm_xor_si128(y, _mm_shuffle_epi32(y, 0xee)),
+        )
+    };
+    let h = unsafe { pmull2(y, x) }; // h = x.hi * y.hi
+    let l = unsafe { pmull(y, x) }; // l = x.lo * y.lo
+    (h, m, l)
+}
+
+/// Karatsuba combine.
+#[inline]
+#[target_feature(enable = "sse2,pclmulqdq")]
+#[allow(unused_unsafe)]
+#[allow(clippy::undocumented_unsafe_blocks, reason = "Too many unsafe blocks.")]
+unsafe fn karatsuba2(h: __m128i, m: __m128i, l: __m128i) -> (__m128i, __m128i) {
+    // Second Karatsuba step: combine into a 2n-bit product.
+    //
+    // m0 ^= l0 ^ h0 // = m0^(l0^h0)
+    // m1 ^= l1 ^ h1 // = m1^(l1^h1)
+    // l1 ^= m0      // = l1^(m0^l0^h0)
+    // h0 ^= l0 ^ m1 // = h0^(l0^m1^l1^h1)
+    // h1 ^= l1      // = h1^(l1^m0^l0^h0)
+    let t = unsafe {
+        //   {m0, m1} ^ {l1, h0}
+        // = {m0^l1, m1^h0}
+        let t0 = {
+            _mm_xor_si128(
+                m,
+                _mm_castps_si128(_mm_shuffle_ps(
+                    _mm_castsi128_ps(l),
+                    _mm_castsi128_ps(h),
+                    0x4e,
+                )),
+            )
+        };
+
+        //   {h0, h1} ^ {l0, l1}
+        // = {h0^l0, h1^l1}
+        let t1 = _mm_xor_si128(h, l);
+
+        //   {m0^l1, m1^h0} ^ {h0^l0, h1^l1}
+        // = {m0^l1^h0^l0, m1^h0^h1^l1}
+        _mm_xor_si128(t0, t1)
+    };
+
+    // {m0^l1^h0^l0, l0}
+    let x01 = unsafe { _mm_unpacklo_epi64(l, t) };
+
+    // {h1, m1^h0^h1^l1}
+    let x23 = unsafe { _mm_castps_si128(_mm_movehl_ps(_mm_castsi128_ps(h), _mm_castsi128_ps(t))) };
+
+    (x23, x01)
+}
+
+/// # Safety
+///
+/// The SSE2 and pclmulqdq target features must be enavled.
+#[inline]
+#[target_feature(enable = "sse2,pclmulqdq")]
+#[allow(unused_unsafe)]
+#[allow(clippy::undocumented_unsafe_blocks, reason = "Too many unsafe blocks.")]
+unsafe fn mont_reduce(x23: __m128i, x01: __m128i) -> __m128i {
+    // Perform the Montgomery reduction over the 256-bit X.
+    //    [A1:A0] = X0 • poly
+    //    [B1:B0] = [X0 ⊕ A1 : X1 ⊕ A0]
+    //    [C1:C0] = B0 • poly
+    //    [D1:D0] = [B0 ⊕ C1 : B1 ⊕ C0]
+    // Output: [D1 ⊕ X3 : D0 ⊕ X2]
+    static POLY: u128 = 1 << 127 | 1 << 126 | 1 << 121 | 1 << 63 | 1 << 62 | 1 << 57;
+    let poly = unsafe { _mm_loadu_si128(ptr::addr_of!(POLY).cast()) };
+    let a = unsafe { pmull(x01, poly) };
+    let b = unsafe { _mm_xor_si128(x01, _mm_shuffle_epi32(a, 0x4e)) };
+    let c = unsafe { pmull2(b, poly) };
+    unsafe { _mm_xor_si128(x23, _mm_xor_si128(c, b)) }
+}
+
+/// Multiplies the low bits in `a` and `b`.
+///
+/// # Safety
+///
+/// The SSE2 and pclmulqdq target features must be enabled.
+#[inline]
+#[allow(unused_unsafe)]
+#[target_feature(enable = "sse2,pclmulqdq")]
+unsafe fn pmull(a: __m128i, b: __m128i) -> __m128i {
+    // SAFETY: This requires the `sse2` and `pclmulqdq` features
+    // which we have.
+    unsafe { _mm_clmulepi64_si128(a, b, 0x00) }
+}
+
+/// Multiplies the high bits in `a` and `b`.
+///
+/// # Safety
+///
+/// The SSE2 and pclmulqdq target features must be enavled.
+#[inline]
+#[allow(unused_unsafe)]
+#[target_feature(enable = "sse2,pclmulqdq")]
+unsafe fn pmull2(a: __m128i, b: __m128i) -> __m128i {
+    // SAFETY: This requires the `sse2` and `pclmulqdq` features
+    // which we have.
+    unsafe { _mm_clmulepi64_si128(a, b, 0x11) }
 }
