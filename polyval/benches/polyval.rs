6473aa0e15bb1dc278afd84f643d0a92125707cc
Tony Arcieri <bascule@gmail.com>
2026-02-07 15:51:12 -0700

    polyval: use `criterion` for benchmarking
    I would like to be able to carefully benchmark when implementing #290,
    especially to compare implementations, and utilize `criterion`'s
    cycle-accurate benchmarks and throughput estimation.

--- polyval/benches/polyval.rs
+++ polyval/benches/polyval.rs
@@ -1,31 +1,44 @@
-//! `polyval` crate benchmarks.
+//! POLYVAL benchmarks.
 
-#![feature(test)]
+#![allow(missing_docs)]
 
-extern crate test;
-
+use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use polyval::{Polyval, universal_hash::UniversalHash};
-use test::Bencher;
 
-// TODO(tarcieri): move this into the `universal-hash` crate
-macro_rules! bench {
-    ($name:ident, $bs:expr) => {
-        #[bench]
-        fn $name(b: &mut Bencher) {
-            let key = Default::default();
-            let mut m = Polyval::new(&key);
-            let data = [0; $bs];
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
+type Benchmarker = Criterion;
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+type Benchmarker = Criterion<criterion_cycles_per_byte::CyclesPerByte>;
 
-            b.iter(|| {
-                m.update_padded(&data);
-            });
+fn bench(c: &mut Benchmarker) {
+    let mut group = c.benchmark_group("polyval");
 
-            b.bytes = $bs;
-        }
-    };
+    for size in &[10, 100, 1000, 10000] {
+        let buf = vec![0u8; *size];
+
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_function(BenchmarkId::new("update_padded", size), |b| {
+            let mut polyval = Polyval::new(&Default::default());
+            b.iter(|| polyval.update_padded(&buf));
+        });
+    }
+
+    group.finish();
 }
 
-bench!(bench1_10, 10);
-bench!(bench2_100, 100);
-bench!(bench3_1000, 1000);
-bench!(bench3_10000, 10000);
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
+criterion_group!(
+    name = benches;
+    config = Criterion::default();
+    targets = bench
+);
+
+#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+criterion_group!(
+    name = benches;
+    config = Criterion::default().with_measurement(criterion_cycles_per_byte::CyclesPerByte);
+    targets = bench
+);
+
+criterion_main!(benches);
