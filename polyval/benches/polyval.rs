5b677b23ceb014be6f3d17dfa78cb049de8f8197
Tony Arcieri <bascule@gmail.com>
2026-02-10 16:40:27 -0700

    polyval: remove `criterion-cycles-per-byte`
    Fixes #302

--- polyval/benches/polyval.rs
+++ polyval/benches/polyval.rs
@@ -5,12 +5,7 @@
 use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use polyval::{Polyval, universal_hash::UniversalHash};
 
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
-type Benchmarker = Criterion;
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-type Benchmarker = Criterion<criterion_cycles_per_byte::CyclesPerByte>;
-
-fn bench(c: &mut Benchmarker) {
+fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("polyval");
 
     for size in &[10, 100, 1000, 10000] {
@@ -27,17 +22,9 @@ fn bench(c: &mut Benchmarker) {
     group.finish();
 }
 
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
 criterion_group!(
     name = benches;
     config = Criterion::default();
-    targets = bench
-);
-
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-criterion_group!(
-    name = benches;
-    config = Criterion::default().with_measurement(criterion_cycles_per_byte::CyclesPerByte);
     targets = bench
 );
 
