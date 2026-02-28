4d64c9a45cc530dc5985d28951bfb07d073a1caf
Tony Arcieri <bascule@gmail.com>
2026-02-28 12:20:19 -0700

    poly1305: rename `poly1305_force_soft` => `poly1305_backend="soft"` (#323)
    Changes the `cfg`-based override to force usage of the soft backend to
    use `poly1305_backend` following a similar change in `polyval` (#259)

--- poly1305/src/backend.rs
+++ poly1305/src/backend.rs
@@ -2,13 +2,13 @@
 
 #[cfg(all(
     any(target_arch = "x86", target_arch = "x86_64"),
-    not(poly1305_force_soft)
+    not(poly1305_backend = "soft")
 ))]
 pub(crate) mod avx2;
 
 #[cfg(all(
     any(target_arch = "x86", target_arch = "x86_64"),
-    not(poly1305_force_soft)
+    not(poly1305_backend = "soft")
 ))]
 pub(crate) mod autodetect;
 
